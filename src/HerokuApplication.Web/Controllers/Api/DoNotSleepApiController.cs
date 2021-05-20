66a88eab7030f86e7763c978838415baa03cc98f
Ilya Kusik <kusikillya2001@gmail.com>
2021-05-20 17:37:00 +0300

    Ready Configuration

---
+++ src/HerokuApplication.Web/Controllers/Api/DoNotSleepApiController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace HerokuApplication.Web.Controllers.Api
+{
+    [ApiController]
+    [Route("/")]
+    public class DoNotSleepApiController : ControllerBase
+    {
+
+        [HttpGet]
+        public string Get()
+        {
+            return "OK";
+        }
+    }
+}
