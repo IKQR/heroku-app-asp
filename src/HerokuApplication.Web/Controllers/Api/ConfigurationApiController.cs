ad706404cf04a4bb2e22063d2ab84f6c5245ab66
Ilya Kusik <kusikillya2001@gmail.com>
2021-05-21 16:59:55 +0300

    configuration

---
+++ src/HerokuApplication.Web/Controllers/Api/ConfigurationApiController.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using HerokuApplication.Web.Attributes;
+using Microsoft.Extensions.Configuration;
+
+namespace HerokuApplication.Web.Controllers.Api
+{
+    //[Route("api/configuration")]
+    [SecretKeyAuth]
+    [ApiController]
+    public class ConfigurationApiController : ControllerBase
+    {
+        private readonly List<KeyValuePair<string, string>> _configList;
+
+        public ConfigurationApiController(IConfiguration configuration)
+        {
+            _configList = configuration.AsEnumerable().ToList();
+        }
+
+        [HttpGet]
+        [Route("/configuration")]
+        public List<KeyValuePair<string, string>> GetAllConfigurations()
+        {
+            return _configList;
+        }
+    }
+}
